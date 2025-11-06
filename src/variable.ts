9b616bebee62d6841d6c448eb097a1a40da45ea1
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-11-07 00:23:24 +0500

    Add initial functions implementation and variable declarations

--- src/variable.ts
+++ src/variable.ts
@@ -1,2 +1,23 @@
 let greeting: String = "Hello, TypeScript!";
-console.log(greeting);
+greeting.toUpperCase();
+// console.log(greeting.toUpperCase());
+
+//Number
+
+let userId = 8798791;
+
+// userId = "Zavyar"
+
+// Boolean
+
+let isLoggedIn : Boolean = true
+
+// Any
+
+let hero;
+
+function herolist(){
+    return "thor"
+}
+
+hero = herolist()
