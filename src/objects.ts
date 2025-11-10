5cfc6e78c890632b4aa57317c42123e50c3820ba
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-11-10 23:34:09 +0500

    Add initial TypeScript object and function templates

---
+++ src/objects.ts
@@ -0,0 +1,17 @@
+// const User = {
+//     name: "Zavyar",
+//     email: "zavyar@123dotcom",
+//     isAvtive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}){}
+
+// let newUser = {name: "Zavyar", isPaid: false, email: "h@h.com"}
+
+// createUser(newUser)
+
+
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactjs", price: 399}
+// }
