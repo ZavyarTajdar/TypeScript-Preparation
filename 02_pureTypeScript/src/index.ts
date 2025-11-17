5131bf23aaff5f2f030560c21d15b5809e6adc49
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-11-17 23:40:29 +0500

    Refactor User class constructor for clarity and remove commented-out code

--- 02_pureTypeScript/src/index.ts
+++ 02_pureTypeScript/src/index.ts
@@ -1,10 +1,22 @@
-class User {
-    constructor(email: string, name: string){
-        this.email = email;
-        this.name = name
-    }
-    email: string;
-    name: string;
-}
+// class User { 
+//     // readonly city: string = "****";
+//     private readonly city: string = "****";
+//     public country: string = "USA";
+//     email: string;
+//     name: string;
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+class User { 
+    constructor(
+        public email: string, 
+        public name: string
+    )
+    {
 
+    }
+}
+// User.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
 const zavyar = new User("zavyar@example.com", "Zavyar");
