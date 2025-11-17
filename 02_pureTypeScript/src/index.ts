6dfbe5a7a524dc66cb57c50d03708e7936f37035
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-11-17 23:52:56 +0500

    getter and setter

--- 02_pureTypeScript/src/index.ts
+++ 02_pureTypeScript/src/index.ts
@@ -10,12 +10,31 @@
 //     }
 // }
 class User { 
+    protected _courseCount = 1
     constructor(
         public email: string, 
         public name: string
     )
     {
 
+    }
+        private deleteToken(){
+        console.log("Token deleted");
+        }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 // User.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
