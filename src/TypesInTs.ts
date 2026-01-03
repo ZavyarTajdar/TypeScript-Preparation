ff27487d9df96b494426e24c0353f0a497d0266e
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2026-01-03 23:34:03 +0500

    Annotation VS Inference

---
+++ src/TypesInTs.ts
@@ -0,0 +1,16 @@
+// Inference
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "TypeScript TV";
+
+// Annotation
+
+let chai: string = "chai";
+
+let numberOfCups: number = 10;
+
+let isTeaReady: boolean = true;
+
+let teaChannel: string = "TypeScript TV";
